3830c8594f6fe4a95c7365ca63852807237a53a2
vickydarlinn <uttapalsangwan@gmail.com>
2024-07-16 14:57:22 +0530

    feat: add logger

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,9 +2,10 @@ import { config } from "dotenv";
 
 config();
 
-const { PORT, NODE_ENV } = process.env;
+const { PORT, NODE_ENV, LOG_LEVEL } = process.env;
 
 export const Config = {
   PORT,
   NODE_ENV,
+  LOG_LEVEL,
 };
--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,19 @@
 import app from "./app";
 import { Config } from "./config";
+import logger from "./config/logger";
 
 const startServer = () => {
   try {
     const PORT = Config.PORT;
     app.listen(PORT, () => {
-      // eslint-disable-next-line no-console
-      console.log(`Listening on port ${PORT}`);
+      logger.info(`Listening on port ${PORT}`);
     });
   } catch (error) {
-    // eslint-disable-next-line no-console
-    console.log(error);
-    process.exit(1);
+    if (error instanceof Error) {
+      logger.error(error.message, () => {
+        process.exit(1);
+      });
+    }
   }
 };
 
