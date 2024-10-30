8850ea452869bc3d5400d07fd0082344d53ccf9d
vickydarlinn <uttapalsangwan@gmail.com>
2024-10-30 19:02:20 +0530

    fix:add global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,5 @@
 import "reflect-metadata";
-import express, { NextFunction, Request, Response } from "express";
-import { HttpError } from "http-errors";
-import logger from "./config/logger";
+import express from "express";
 import authRouter from "./routes/auth";
 import restaurantRouter from "./routes/restaurant";
 import userRouter from "./routes/user";
@@ -9,9 +7,14 @@ import path from "path";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import { Config } from "./config";
+import { globalErrorHandler } from "./routes/globalErrorHandler";
 
 const app = express();
-const ALLOWED_DOMAINS = [Config.CLIENT_UI_DOMAIN, Config.ADMIN_UI_DOMAIN];
+const ALLOWED_DOMAINS = [
+  Config.CLIENT_UI_DOMAIN,
+  Config.ADMIN_UI_DOMAIN,
+  "http://localhost:5174",
+];
 app.use(
   cors({
     origin: ALLOWED_DOMAINS as string[],
@@ -38,26 +41,27 @@ app.get("/test", (req, res, next) => {
   }
 });
 
+app.use(globalErrorHandler);
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
-  let message = "Internal server error";
-  const statusCode = err.statusCode || err.status || 500;
+// app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+//   let message = "Internal server error";
+//   const statusCode = err.statusCode || err.status || 500;
 
-  if (statusCode === 400) {
-    message = err.message;
-  }
-  logger.error(err.message);
+//   if (statusCode === 400) {
+//     message = err.message;
+//   }
+//   logger.error(err.message);
 
-  res.status(statusCode).json({
-    errors: [
-      {
-        type: err.name,
-        msg: message,
-        path: req.path,
-        method: req.method,
-        location: "server",
-      },
-    ],
-  });
-});
+//   res.status(statusCode).json({
+//     errors: [
+//       {
+//         type: err.name,
+//         msg: message,
+//         path: req.path,
+//         method: req.method,
+//         location: "server",
+//       },
+//     ],
+//   });
+// });
 export default app;
